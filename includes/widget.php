6d842e0846f01733fe833456272834bb48d73f19
Byggvir <thomas@arend-rhb.de>
2018-12-30 18:18:08 +0100

    Umgeschreiben von Ev. Termine aus Losungen

--- includes/widget.php
+++ includes/widget.php
@@ -34,40 +34,39 @@ require_once(sprintf("%s/postprocess.php", dirname(__FILE__)));
  Calendar Widget for XML interface to "Evangelische Termine" database
 **************************************************************************/
  
-class EVT_Calendar_Widget extends WP_Widget {
+class HHL_Losung_Widget extends WP_Widget {
 	
 	/** constructor */
 	function __construct() {
 		parent::WP_Widget( 
-			/* Base ID */ 'evt_calendar_widget' ,
-			/* Name */ 'EVT Calendar Widget' ,
-			array( 'description' => 'Zeigt eine &Uuml;bersicht der Termine eines Veranstalters aus dem Evangelischen Termine Kalender in einem Widget an! ' ) );
+			/* Base ID */ 'hhl_losungen_widget' ,
+			/* Name */ 'HHL Losungen Widget' ,
+			array( 'description' => 'Zeigt die Losung des Tages an.' ) );
 	}
 
 	/** @see WP_Widget::widget */
 	function widget( $args, $instance ) {
 
       	global
-	  $EVT_DefLabels ,
-      	  $EVT_CalenderSettingNames, 
-      	  $EVT_ServerSettingNames,
-      	  $EVT_QuerySettingNames ,
-      	  $EVT_DefValues;
+          $HHL_DefLabels ,
+      	  $HHL_DownloadSettingNames, 
+      	  $HHL_LosungenSettingNames,
+      	  $HHL_DefValues;
  
 		$wg_atts = $args;
 
 		foreach ( $instance as $key => $value) {
-			$wg_atts[$key] = Trim(empty($instance[$key]) ? $EVT_DefValues[$key] : $instance[$key]);
+			$wg_atts[$key] = Trim(empty($instance[$key]) ? $HHL_DefValues[$key] : $instance[$key]);
 		}	
 		$wg_atts['title'] = apply_filters('widget_title', $wg_atts['title']);
-		if ($wg_atts['script'] == '' ) $wg_atts['script'] = 'xml.php';
-		$termine=postprocess_xml(evt_getevents($wg_atts,$EVT_QuerySettingNames),'widget');
+		//if ($wg_atts['script'] == '' ) $wg_atts['script'] = 'xml.php';
+		// $termine=postprocess_xml(hhl_getlosungen($wg_atts,$HHL_LosungenSettingNames),'widget');
 
 		echo $before_widget;
 		if ( $wg_atts['title'] )
 			echo $before_title . $wg_atts['title'] . $after_title;
 			echo $termine; // Hier ist der richtige Patz um den erzeugten HTML Code auszugeben. 
-			echo "<p style=\"font-size: x-small;\">Powered by Evangelische Termine Plugin - &copy Thomas Arend, Rheinbach</p>";
+			echo "<p style=\"font-size: x-small;\">Powered by WP Losungen SQL Plugin - &copy Thomas Arend, Rheinbach</p>";
 
 		echo $after_widget;
 	}
@@ -76,14 +75,13 @@ class EVT_Calendar_Widget extends WP_Widget {
 	function update( $new_instance, $old_instance ) {
 
       	global
-	  $EVT_DefLabels ,
-      	  $EVT_CalenderSettingNames, 
-      	  $EVT_ServerSettingNames,
-      	  $EVT_QuerySettingNames ,
-      	  $EVT_DefValues;
+          $HHL_DefLabels ,
+      	  $HHL_DownloadSettingNames, 
+      	  $HHL_LosungenSettingNames,
+      	  $HHL_DefValues;
   
 		$instance = $old_instance;
-	        foreach ( $EVT_DefValues as $key => $value ) {
+	        foreach ( $HHL_DefValues as $key => $value ) {
         		$instance[$key] = strip_tags($new_instance[$key]);
 		}
      
@@ -94,30 +92,29 @@ class EVT_Calendar_Widget extends WP_Widget {
 	function form( $instance ) {
 
       	global
-	  $EVT_DefLabels ,
-      	  $EVT_CalenderSettingNames, 
-      	  $EVT_ServerSettingNames,
-      	  $EVT_QuerySettingNames ,
-      	  $EVT_DefValues;
+          $HHL_DefLabels ,
+      	  $HHL_DownloadSettingNames, 
+      	  $HHL_LosungenSettingNames,
+      	  $HHL_DefValues;
  		
 		if ( $instance ) {
-			$instance = wp_parse_args( (array) $instance, $EVT_DefValues ); 
-	        	foreach ( $EVT_DefValues as $key => $value ) {
+			$instance = wp_parse_args( (array) $instance, $HHL_DefValues ); 
+	        	foreach ( $HHL_DefValues as $key => $value ) {
    				$$key	= esc_attr( $instance[ $key ] );
 			}
 			
 		}
 		else {
-	        	foreach ( $EVT_DefValues as $key => $value ) {
+	        	foreach ( $HHL_DefValues as $key => $value ) {
 				$$key	= __( $value , 'text_domain' );
 			}
 		}
 		echo '<p>' ;
 
-		foreach ( $EVT_DefValues as $key => $value ) {
+		foreach ( $HHL_DefValues as $key => $value ) {
 		?>
 		
-		<label for="<?php echo $this->get_field_id($key); ?>"><?php _e($EVT_DefLabels[$key].':'); ?></label> 
+		<label for="<?php echo $this->get_field_id($key); ?>"><?php _e($HHL_DefLabels[$key].':'); ?></label> 
 		<input class="widefat" id="<?php echo $this->get_field_id($key); ?>" name="<?php echo $this->get_field_name($key); ?>" type="text" value="<?php echo $$key; ?>" />
 
 		<?php
@@ -126,10 +123,10 @@ class EVT_Calendar_Widget extends WP_Widget {
 
 	}
 
-} // class EVT_Calendar_Widget
+} // class HHL_Losungen_Widget
 
-// register EVT_Calendar_Widget
+// register HHL_Losungen_Widget
 
-add_action( 'widgets_init', create_function( '', 'register_widget("EVT_Calendar_Widget");' ) );
+add_action( 'widgets_init', create_function( '', 'register_widget("HHL_Losungen_Widget");' ) );
 
 ?>
